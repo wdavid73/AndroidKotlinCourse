b29dbfd04e0a3d806e231ed95902c753663a90b0
Wilson Padilla <wdavidw26@gmail.com>
2024-05-27 11:57:04 -0500

    add paddings

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -18,9 +18,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -74,8 +79,10 @@ fun ArtSpaceLayout(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom,
         modifier = modifier
-            .padding(WindowInsets.navigationBars.asPaddingValues())
-            .padding(WindowInsets.statusBars.asPaddingValues())
+            .navigationBarsPadding()
+            .statusBarsPadding()
+            .safeDrawingPadding()
+            .verticalScroll(rememberScrollState())
 
     ) {
         ArtImage(
